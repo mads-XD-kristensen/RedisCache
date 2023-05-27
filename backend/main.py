5bf28f778e8bf05a9668b22986ba841a23975697
Mads Kristensen <madsarke@gmail.com>
2023-05-27 13:37:30 +0200

    dataene er nu en liste af dicitonary gemt i redis som string

--- backend/main.py
+++ backend/main.py
@@ -31,41 +31,29 @@ def search(start: str, stop: str):
     if output: #Hvis redis allerede har en cachet route
         return {output}
     else:
-        #gds = GraphDataScience(NEOURL, auth=(NEOUSER, NEOPASS), database="neo4j")
-        cypher_query = """
-        MATCH (a:Stop), (b:Stop)
-        WHERE a.name = '$start' AND b.name = '$stop'
-        WITH a,b
-        MATCH p = allshortestpaths((a)-[*]-(b))  
-        WHERE NONE (x IN RELATIONSHIPS(p) WHERE type(x)='OPERATES')  
-        RETURN nodes(p) LIMIT 1
-        """.format(rstart=start, rstop=stop)
-        #print(cypher_query)
-        #output = gds.run_cypher(cypher_query) # Hent fra database her og put i cache
-        #print(output)
-        #print("\n")
-        #df = pd.DataFrame(output)#, columns=output
-        #print(df.to_string())
-        #gds.close()
         with gd.driver(NEOURL, auth=(NEOUSER, NEOPASS)) as driver:
             driver.verify_connectivity()
-            #print("im in")
+            full_route = []
+
             records, summary, keys = driver.execute_query(
                 "MATCH (a:Stop), (b:Stop) WHERE a.name = $start AND b.name = $stop WITH a,b MATCH p = allshortestpaths((a)-[*]-(b)) WHERE NONE (x IN RELATIONSHIPS(p) WHERE type(x)='OPERATES') RETURN nodes(p) LIMIT 1",
                 start=start, stop=stop
             )
-            #print("The query `{query}` returned {records_count} records in {time} ms.".format(
-            #query=summary.query, records_count=len(records),
-            #time=summary.result_available_after,
-            #))
-            #print(type(records))
             if len(records) > 0:
-                for subway in records:
-                    #print(f"{subway.data()}")
-                    output = str(subway.data())
-                    r.set(route, output, 60) #cache i redis db med den søgte route som key og selve routen som value, bliver i redis db i 10 min
+                for subway in records: #Dette for-loop er for at få dataene ud af det dictionary man får fra neo4j, neo4j giver et dictionary med 1 key som hedder nodes(p) og valuen er en liste af dictionary 
+                    output = subway.data()
+                    output = output["nodes(p)"]
+
+                for route_stop in output: #Dette for-loop er for at gå igennem den liste af dictionary og hente values ud
+                    
+                    full_route.append(route_stop.items())
+                    #print(str(full_route))
+                
+                full_route_string = str(full_route)
+                r.set(route, full_route_string, 60) #cache i redis db med den søgte route som key og selve routen som value, bliver i redis db i 10 min
+
             else:
                 output = "No route exists"
-
-        return {output, route, " was not in cache but was in db, is now in cache for 10 min"} #placeholder
-    
+                return {output}
+            
+    return {full_route_string, route, " was not in cache but was in db, is now in cache for 10 min"} #placeholder
